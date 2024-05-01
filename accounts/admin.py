f06256111ab7942c415ce3ef62ff8aed081a30b6
Park, Su Eun <park_sueun@naver.com>
2024-05-02 00:08:36 +0900

    UserAdmin list_display 명시

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,4 +5,4 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['username', 'email', 'website_url', 'is_active', 'is_staff', 'is_superuser']
