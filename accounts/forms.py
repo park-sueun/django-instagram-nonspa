4af6e47f75a5077c0c4a4b327cd048e280cd2f34
Park, Su Eun <park_sueun@naver.com>
2024-05-02 00:08:05 +0900

    회원가입 구현하기

---
+++ accounts/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ValidationError
+from .models import User
+
+class SignupForm(UserCreationForm):
+    
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = (
+            'username', 'email', 'first_name', 'last_name'
+        )
+        
+    def clean_email(self):
+        """Reject usernames that differ only in case."""
+        email = self.cleaned_data.get("email")
+        if email:
+            queryset = User.objects.filter(email=email)
+            if queryset.exists():
+                raise ValidationError("이미 등록된 이메일 주소입니다.")
+        
+        return email
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import SignupForm
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, '회원가입 환영합니다.')
+            return redirect('/')
+    else:
+        form = SignupForm()
+        
+    return render(request, 'accounts/signup_form.html', {
+        'form': form,
+    })
