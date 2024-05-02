ad446b29789b73a3a1adf01fe4a7a86c877a5fe6
Park, Su Eun <park_sueun@naver.com>
2024-05-02 22:48:54 +0900

    profile_edit 함수 기반 뷰 생성

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, resolve_url
 from django.contrib.auth.views import LoginView, logout_then_login, LogoutView
 from django.contrib.auth import login as auth_login
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.conf import settings
-from .forms import SignupForm
+from .forms import SignupForm, ProfileForm
 
 login = LoginView.as_view(
     template_name = 'accounts/login.html',
@@ -32,4 +33,19 @@ def signup(request):
         
     return render(request, 'accounts/signup_form.html', {
         'form': form,
+    })
+    
+@login_required
+def profile_edit(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, '프로필 수정이 완료되었습니다.')
+            return redirect("profile_edit")
+    else:
+        form = ProfileForm(instance=request.user)
+            
+    return render(request, 'accounts/profile_edit_form.html', {
+        'form': form
     })
