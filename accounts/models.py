252fe8d58afe3b81fc52e769677a11d7d9280161
Park, Su Eun <park_sueun@naver.com>
2024-05-02 23:55:48 +0900

    added fields(phone_number, gender) & migrate

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    class GENDER(models.TextChoices):
+        MALE = "M", "Male"
+        FEMALE = "F", "Female"
+    
     website_url = models.URLField(blank=True)
     bio = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=13, blank=True,
+                                    validators=[RegexValidator(r"^010-?[1-9]\d{3}-?\d{4}$")])
+    gender = models.CharField(max_length=1, blank=True, choices=GENDER)
