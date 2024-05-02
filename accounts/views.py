c3688601d50ea643a5557dc8cbaa3bcf5118a420
Park, Su Eun <park_sueun@naver.com>
2024-05-02 21:48:41 +0900

    login, logout view 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, resolve_url
+from django.contrib.auth.views import LoginView, logout_then_login, LogoutView
+from django.contrib.auth import login as auth_login
 from django.contrib import messages
+from django.conf import settings
 from .forms import SignupForm
+
+login = LoginView.as_view(
+    template_name = 'accounts/login.html',
+)
+
+logout = LogoutView.as_view(
+    http_method_names = ['get'],
+    next_page = settings.LOGIN_URL
+)
+
+# def logout(request):
+#     messages.success(request, '로그아웃 되었습니다.')
+#     return logout_then_login(request)
 
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
+            signed_user = form.save()
+            auth_login(request, signed_user)
             messages.success(request, '회원가입 환영합니다.')
             return redirect('/')
     else:
