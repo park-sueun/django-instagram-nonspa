1639760a0cd37e9598899122f47457af79341e27
Park, Su Eun <park_sueun@naver.com>
2024-05-03 21:59:03 +0900

    create PasswordChangeForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,7 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    PasswordChangeForm as AuthPasswordChangeForm
+)
 from django.forms import ValidationError, ModelForm
 from .models import User
 
@@ -26,3 +29,13 @@ class ProfileForm(ModelForm):
         fields = (
             'profile', 'first_name', 'last_name', 'gender', 'phone_number', 'website_url', 'bio'
         )
+        
+class PasswordChangeForm(AuthPasswordChangeForm):
+    def clean_new_password1(self):
+        old_password = self.cleaned_data.get('old_password')
+        new_password1 = self.cleaned_data.get('new_password1')
+        
+        if old_password == new_password1:
+            raise ValidationError("새로운 암호는 기존 암호와 다르게 입력해주세요.")
+        
+        return new_password1
