607b227bdeb677fc94330e6dfd7e0ff7cc0ad80b
Park, Su Eun <park_sueun@naver.com>
2024-05-03 22:13:10 +0900

    accounts app_name 지정

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('login/', views.login, name='login'), # /accounts/login/ => settings.LOGIN_URL
     path('logout/', views.logout, name='logout'),
